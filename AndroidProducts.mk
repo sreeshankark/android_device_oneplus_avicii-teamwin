fc3e6dab60d15c25f07e7b7aa671f63f4c977fcf
Sanju0910 <sreeshankar0910@gmail.com>
2022-11-21 13:19:56 +0530

    avicii: Changed some copyright details

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
 # Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 TeamWin Recovery Project 
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/orangefox_avicii.mk
+    $(LOCAL_DIR)/teamwin_avicii.mk
 
 COMMON_LUNCH_CHOICES := \
-    orangefox_avicii-eng
+    teamwin_avicii-eng
